69757ab97f7ddd7b0c16551e132f68e8cfae9e23
S_Nathan22 <121171371+Y-Cahino@users.noreply.github.com>
2025-01-13 10:35:04 +0100

    Update autosalone.sql
    rimozione duplicato

--- autosalone.sql
+++ autosalone.sql
@@ -1,16 +1,5 @@
 CREATE DATABASE IF NOT EXISTS autosaloni;
 USE autosaloni;
-CREATE TABLE `autosalone` (
-  `bunghID` int(11) NOT NULL,
-  `Modello` varchar(20) NOT NULL,
-  `Anno di Produzione` int(4) NOT NULL,
-  `Prezzo` int(11) NOT NULL,
-  `Marca` varchar(20) NOT NULL,
-  `Nuova o Usata` tinyint(1) NOT NULL,
-  `Test Drive` tinyint(1) NOT NULL,
-  `Promo` int(3) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
 --
 -- Dump dei dati per la tabella `autosalone`
 --
